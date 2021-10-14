9bd4cdb186fffc1da65a327de648765bb4b302fb
yaminirayapati <92515897+yaminirayapati@users.noreply.github.com>
2021-10-14 23:20:01 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,17 @@
 
+def area(circle):
+    return area(pi*(r*r))
+
+pi=3.14
+r=2
+circle=pi*r*r
+print(circle)
+
+
+
+
+ def extension(file):
+    return extension("filename.split")
+filename="yamini.py"
+file=filename.split(".")[-1]
+print(file)

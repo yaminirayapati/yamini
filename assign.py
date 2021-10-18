90f14ee73d0df183e7da6f096e8cfc420051b13d
yaminirayapati <92515897+yaminirayapati@users.noreply.github.com>
2021-10-18 12:09:40 +0530

    Create assign.py

---
+++ assign.py
@@ -0,0 +1,34 @@
+a = 5
+b = 9
+print(a<b)
+print(a<=b)
+print(a>b)
+print(a>=b)
+
+
+def add(x,y):
+  return x+y
+def subtract(x,y):
+  return x-
+print("select operation.")
+print("1.add")
+print("2.subtract")
+choice = input("enter choice(1/2):")
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
+if choice == '1':
+  print(num1, "+", num2, add(num1,num2))
+if choice == '2':
+  print(num1, "-", num2, add(num1,num2))
+else:
+  print("invalid input")
+  
+  
+  
+  
+  given series 10,1,8,3,6,5,4,7,x,y
+  step1 : start
+  step2 : let name the series as A,B,C,D,E,F,G,H,X,Y
+  Step3 : X=9-H
+  step4: y=11-x
+  step5: stop
